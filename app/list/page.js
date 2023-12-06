6f78ce6934db7e0901703426b66156065a0e4e2e
dam1007 <dam1007@naver.com>
2023-12-06 21:43:03 +0900

    8. 캐싱 중간커밋

--- app/list/page.js
+++ app/list/page.js
@@ -3,6 +3,9 @@
 import { connectDB } from "@/util/database.js";
 import ListItem from "./ListItem";
 
+// static rendering 다이나믹 렌더링으로 바꾸기
+export const dynamic = 'force-dynamic'
+
 export default async function List() {
     const client = await connectDB;
     let db = client.db("forum");
--- app/page.js
+++ app/page.js
@@ -3,11 +3,21 @@ const { MongoClient } = require('mongodb');
 
 export default async function Home() {
 
-  const client = await connectDB;
+  /* const client = await connectDB;
   const db = client.db("forum");
-  let result = await db.collection('post').find().toArray();
+  let result = await db.collection('post').find().toArray(); */
 
+  // GET요청 결과 캐싱하기
+  await fetch('/URL', {cache: 'force-cache'})
+  // 주의! await fetch('/URL')만 써도 캐싱됨.
+
+  // 2. 실시간 데이터가 필요해 캐싱하면 안될 경우
+  // await fetch('/URL', {cache: 'no-store'})
+
+  // 3. 60초마다 캐싱된 데이터 갱신해줌
+  // await fetch('/URL', {next : {revalidate : 60}})
+  
   return (
-    <div>{result[0].title}</div>
+    <div>{/* {result[0].title} */}</div>
   )
 }
